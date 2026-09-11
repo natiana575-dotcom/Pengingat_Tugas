cfbfecb29bbce00c8f2b6e0ad33957ec936f4f7f
natiana575-dotcom <natiana575@gmail.com>
2026-09-11 09:59:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -732,7 +732,7 @@ if st.session_state.page == "Beranda":
 
     st.markdown(
         dedent(f"""<div class="header-card"><div class="current-date">{tanggal_indonesia(today, True)}
-            </div><div class="greeting">Satu - satu, selesai/div><div class="description">Catat yang perlu dikerjakan. Biar kepala lebih lega dan deadline terasa lebih dekat untuk ditaklukan.</div><div class="stats"><span class="completed-stat">Tugas selesai: {len(completed)}</span><span class="incomplete-stat">Tugas belum selesai: {len(unfinished)}</span></div></div>"""),
+            </div><div class="greeting">Satu - satu, selesai<div class="description">Catat yang perlu dikerjakan. Biar kepala lebih lega dan deadline terasa lebih dekat untuk ditaklukan.</div><div class="stats"><span class="completed-stat">Tugas selesai: {len(completed)}</span><span class="incomplete-stat">Tugas belum selesai: {len(unfinished)}</span></div></div>"""),
         unsafe_allow_html=True
     )
 
