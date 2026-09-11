235cc722f91a75a0d9cb5d87ebac164910332357
natiana575-dotcom <natiana575@gmail.com>
2026-09-11 09:57:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -731,40 +731,13 @@ if st.session_state.page == "Beranda":
     ]
 
     st.markdown(
-        dedent(f"""
-        <div class="header-card">
-            <div class="current-date">
-                {tanggal_indonesia(today, True)}
-            </div>
-
-            <div class="greeting">
-                Satu - satu, selesai
-            </div>
-
-            <div class="description">
-                Catat yang perlu dikerjakan.
-                Biar kepala lebih lega dan deadline terasa
-                lebih dekat untuk ditaklukan.
-            </div>
-
-            <div class="stats">
-                <span class="completed-stat">
-                    Tugas selesai: {len(completed)}
-                </span>
-
-                <span class="incomplete-stat">
-                    Tugas belum selesai: {len(unfinished)}
-                </span>
-            </div>
-        </div>
-        """),
+        dedent(f"""<div class="header-card"><div class="current-date">{tanggal_indonesia(today, True)}
+            </div><div class="greeting">Satu - satu, selesai/div><div class="description">Catat yang perlu dikerjakan. Biar kepala lebih lega dan deadline terasa lebih dekat untuk ditaklukan.</div><div class="stats"><span class="completed-stat">Tugas selesai: {len(completed)}</span><span class="incomplete-stat">Tugas belum selesai: {len(unfinished)}</span></div></div>"""),
         unsafe_allow_html=True
     )
 
     if st.session_state.show_add_task:
-
-        st.markdown(
-            '<div class="section-title">Tambah Tugas</div>',
+        st.markdown('<div class="section-title">Tambah Tugas</div>',
             unsafe_allow_html=True
         )
 
