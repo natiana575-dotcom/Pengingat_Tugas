53e715eac37102d7e9dd8a09c10b8e8c7f0a3a47
natiana575-dotcom <natiana575@gmail.com>
2026-09-12 12:25:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -976,10 +976,10 @@ if st.session_state.page == "Beranda":
                 card_col, button_col = st.columns([10, 1])
                 with card_col:
                     
-    st.markdown(
-        dedent(f"""<div class="task-card"><div class="subject-text">{subject_name}</div><div class="task-text">{task_name}</div><div class="deadline-text">Tenggat:{tanggal_indonesia(task["deadline"])}</div></div>"""),
-        unsafe_allow_html=True
-    )
+                    st.markdown(
+                        dedent(f"""<div class="task-card"><div class="subject-text">{subject_name}</div><div class="task-text">{task_name}</div><div class="deadline-text">Tenggat:{tanggal_indonesia(task["deadline"])}</div></div>"""),
+                        unsafe_allow_html=True
+                    )
 
 with button_col:
 
